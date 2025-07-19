db62a560e18c08da5db7c24724d3e4f0a6ced2b4
draines21 <draines1992@outlook.com>
2025-07-19 14:13:19 -0700

    Initial Commit for asteroids

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame 
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+
+
+def main():
+    print("Starting Asteroids!")
+    print("Screen width:",  SCREEN_WIDTH)
+    print("Screen height:", SCREEN_HEIGHT)
+
+
+if __name__ == "__main__":
+    main()
